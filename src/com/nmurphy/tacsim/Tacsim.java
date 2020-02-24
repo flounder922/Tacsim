39b57c800281cd5af8c83dbc4498865539fd388f
Nathaniel Murphy <floundeer922@gmail.com>
2020-02-24 15:12:51 -0800

    added memory dump print out

--- src/com/nmurphy/tacsim/Tacsim.java
+++ src/com/nmurphy/tacsim/Tacsim.java
@@ -21,12 +21,10 @@ public class Tacsim {
         {
             FileInputStream fr = new FileInputStream(new File(args[0]));
             int i = 0;
-            int data = 0;
-            while((data = fr.read()) != -1)
-            {
+            int data;
+            while((data = fr.read()) != -1) {
                 memory_array[i] = data;
             }
-            data = 0;
         }
         catch (ArrayIndexOutOfBoundsException ex)
         {
@@ -43,6 +41,7 @@ public class Tacsim {
         while(cycles < 10000)
         {
             if (memory_array[PP] == 0) {
+                PP++;
             } else if (memory_array[PP] == 1) {
                 PP++;
                 ACC += memory_array[PP];
@@ -65,7 +64,7 @@ public class Tacsim {
                 ACC = memory_array[PP];
             } else if (memory_array[PP] == 8) {
                 PP++;
-                ACC = memory_array[PP + memory_array[PP]];
+                ACC = memory_array[PP + X];
             } else if (memory_array[PP] == 9) {
                 PP++;
                 X = ACC;
@@ -100,12 +99,22 @@ public class Tacsim {
         }
 
         // Do a Memory-Dump
-        for(int i = 0; i < memory_array.length; ++i) {
+        /*for(int i = 0; i < memory_array.length; ++i) {
             System.out.print(memory_array[i]);
             if (i == 15 || i == 31 || i == 47 || i == 63 || i == 79 || i == 95 || i == 111 || i == 127 || i == 143 || i == 159
             || i == 175 || i == 191 || i == 207 || i == 223 || i == 239 || i == 255){
                 System.out.println();
             }
+        }*/
+        int row = 0;
+        int j = 0;
+        System.out.println("Memory Dump");
+        for(int i = 0; i < 16; ++i) {
+            j = 0;
+            for (; j < 16; ++j) {
+                System.out.print(memory_array[j + row]);
+            }
+            row += j + 1;
         }
     }
 }
